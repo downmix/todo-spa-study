6185a26d0393b81dd6f30fdb5889994ffb0fcfb7
downmix <downmix@downmix.net>
2017-09-07 13:28:50 +0900

    로그인 기본구현

--- public/index.js
+++ public/index.js
@@ -1,5 +1,8 @@
-(function (window, document) {
 
+let userId = '';
+let userName = '';
+
+(function (window, document) {
   /**
    * 서버에서 할일 템플릿과 할일 데이터를 가져온 후, #todos 요소 안에 렌더링하는 함수
    */
@@ -17,10 +20,17 @@
         // 낙관적 업데이트
         const checkboxEl = todoItem.querySelector('.todo-checkbox')
         checkboxEl.addEventListener('click', e => {
-          axios.patch(`/api/todos/${id}`, {
+          authedAxios.patch(`/api/todos/${id}`, {
             complete: e.currentTarget.checked
           }).then(res => {
             loadTodos()
+          }).catch(err => {
+            if(err.response.status === 401){
+              console.log(err.response, '<< [ err.response ]');
+              statusInfo('할일체크 하기 위해서는 로그인이 필요합니다');
+              
+            }
+            //console.log(err.response.status, '<< [ err ]');
           })
         })
 
@@ -28,12 +38,33 @@
         // 비관적 업데이트
         const removeLink = todoItem.querySelector('.todo-remove')
         removeLink.addEventListener('click', e => {
-          axios.delete(`/api/todos/${id}`).then(res => {
+          authedAxios.delete(`/api/todos/${id}`).then(res => {
             loadTodos()
           })
         })
       })
     })
+  }
+
+  function loginArea() {
+    if(sessionStorage.getItem('my-todo-app-token')){
+      render({
+        target: '#login',
+        templatePath: '/templates/login-ok.ejs',
+        dataPath: '/api/auth'
+      })
+      .then((templat) => {
+        authedAxios = axios.create({
+          headers: {
+            'Authorization': `Bearer ${sessionStorage.getItem('my-todo-app-token')}`
+          }
+        })
+  
+        user = JSON.parse(sessionStorage.getItem('my-todo-app'))
+        templat.querySelector("span#loginOkId").textContent = user.id
+        templat.querySelector("span#loginOkName").textContent = user.name
+      })
+    }
   }
 
   document.querySelector('#todo-form').addEventListener('submit', e => {
@@ -42,15 +73,59 @@
     //e.stopPropagation()
     //이벤트 버블링 중단
     const form = e.currentTarget
-    axios.post('/api/todos', {
+    authedAxios.post('/api/todos', {
       title: form.elements.title.value
     })
       .then(loadTodos)
       .then(() => {
-        form.elements.title.value = null
+        //form.elements.title.value = null
+      })
+  })
+
+  document.querySelector('#login-form').addEventListener('submit', e => {
+    e.preventDefault()
+    const form = e.currentTarget
+    axios.post('/api/login', {
+      id: form.elements.loginId.value,
+      password: form.elements.loginPw.value
+    })
+      .then((res) => {
+        const user = {
+          id: res.data.id, 
+          name: res.data.name
+        }
+
+        sessionStorage.setItem('my-todo-app-token', res.data.token)
+        sessionStorage.setItem('my-todo-app', JSON.stringify(user))
+        loginArea()
       })
   })
 
   loadTodos()
+  loginArea()
 
 })(window, document)
+
+let user;
+let authedAxios = axios.create({
+  headers: {
+    'Authorization': `Bearer ${sessionStorage.getItem('my-todo-app-token')}`
+  }
+})
+
+const logoutBtn = () => {
+  sessionStorage.removeItem('my-todo-app-token')
+  sessionStorage.removeItem('my-todo-app')
+  location.href = '/'
+}
+
+const statusInfo = msg => {
+  //document.querySelector('#status-info').textContent = msg
+  const infoEl = document.createElement('h3')
+  infoEl.textContent = msg
+  document.querySelector('#status-info').appendChild(infoEl);
+
+  (setTimeout(function() {
+    infoEl.textContent = ''
+  }, 3000));
+}
--- server.js
+++ server.js
@@ -9,6 +9,27 @@ const data = require('./data')
 const app = express()
 const jsonMiddleware = bodyParser.json()
 
+/*------------------------------------*/
+/* [ PWT 로그인선언 ] */
+const jwt = require('jsonwebtoken') //JWT의 생성과 검증을 위한 패키지 
+const expressJwt = require('express-jwt') //JWT가 요청에 포함되어 서버에 들어왔을 때, 해당 토큰을 검증 및 변환해서 `req.user`에 저장해주는 express 미들웨어
+
+const SECRET = 'appsecretkey' //토큰 서명의 서버 비밀 키
+const authMiddleware = expressJwt({secret: SECRET}) //인증 미들웨어
+
+const users = [
+  {
+    id: 'asdf',
+    name: '사용자1',
+    password: '1234',
+  },
+  {
+    id: 'foo',
+    name: '사용자2',
+    password: 'bar'
+  }
+]
+
 app.use(morgan('tiny'))
 app.use(express.static('public'))
 
@@ -20,7 +41,8 @@ app.get('/api/todos', (req, res) => {
 
 /*------------------------------------*/
 /* [ todo 신규추가 ] */
-app.post('/api/todos', jsonMiddleware, (req, res) => {
+app.post('/api/todos', jsonMiddleware, authMiddleware, (req, res) => {
+  console.log(req.user, '<< [ req.user ]');
   const {title} = req.body
   if (title) {
     const todo = data.addTodo({title})
@@ -33,7 +55,7 @@ app.post('/api/todos', jsonMiddleware, (req, res) => {
 
 /*------------------------------------*/
 /* [ todo id >> 수정 ] */
-app.patch('/api/todos/:id', jsonMiddleware, (req, res) => {
+app.patch('/api/todos/:id', jsonMiddleware, authMiddleware, (req, res) => {
   let id;
   try {
     id = parseInt(req.params.id)
@@ -48,7 +70,7 @@ app.patch('/api/todos/:id', jsonMiddleware, (req, res) => {
 
 /*------------------------------------*/
 /* [ todo id >> 삭제 ] */
-app.delete('/api/todos/:id', jsonMiddleware, (req, res) => {
+app.delete('/api/todos/:id', jsonMiddleware, authMiddleware, (req, res) => {
   let id;
   try {
     id = parseInt(req.params.id)
@@ -61,8 +83,38 @@ app.delete('/api/todos/:id', jsonMiddleware, (req, res) => {
   res.end()
 })
 
+/*------------------------------------*/
+/* [ 로그인 인증 ] */
+app.post('/api/login', jsonMiddleware, (req, res) => {
+  console.log(req.body, '<< [ req.body ]')
+  const {id, password} = req.body
+  const findUser = users.find(user => {
+    return user.id === id && user.password === password
+  })
+  
+  if(findUser){
+    const token = jwt.sign({id}, SECRET)
+    res.send({
+      ok: true,
+      id: findUser.id,
+      name: findUser.name,
+      token
+    })
+  }else{
+    res.status(400)
+    res.send({
+      ok: false,
+    })
+  }
+})
+
+/*------------------------------------*/
+/* [ 로그인 검증 ] */
+app.get('/api/auth', jsonMiddleware, authMiddleware, (req, res) => {
+  console.log(req.user, '<< [ req.user ]');
+  res.send(req.user)
+})
+
 app.listen(3000, () => {
   console.log('listening...')
 })
-
-// login브랜치 테스트
